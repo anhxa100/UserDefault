180c58c9782c71b059131e2de9b69a1ee80b2d2e
anhxa100 <anhxa100@gmail.com>
2018-10-03 10:48:17 +0700

    no message

---
+++ UserDefault/UserDefault.swift
@@ -0,0 +1,12 @@
+//
+//  UserDefault.swift
+//  UserDefault
+//
+//  Created by anhxa100 on 10/3/18.
+//  Copyright © 2018 anhxa100. All rights reserved.
+//
+
+import Foundation
+import  UIKit
+
+
---
+++ UserDefault/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UserDefault
+//
+//  Created by anhxa100 on 10/3/18.
+//  Copyright © 2018 anhxa100. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let defaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        defaults.set(20, forKey: "my Age")
+        print(defaults.integer(forKey: "my Age"))
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
